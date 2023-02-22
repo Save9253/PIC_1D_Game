88e62570f64b9ec0aabfe89ba8f2ae0ae0b6b809
Саунин Савелий <save9253@mail.ru>
2023-02-22 15:23:40 -0500

    modified functions

--- newmainXC16.c
+++ newmainXC16.c
@@ -9,41 +9,44 @@
 #define FCY 3700000UL
 #include <libpic30.h>
 
-int main(void)
+void blink(unsigned char ledNumber)
 {
-    AD1PCFGL = 0xFFFF;
-    TRISA = 0xFFFF;
-    TRISB = 0x0000;
-    LATB = 0xFFFF; // all are off
+    // turn on the last LED for half a second then turn off
+    LATB &= ~(1<<ledNumber);
+    __delay_ms(500);
+    LATB |= (1<<ledNumber);
+}
 
-    void processInput(unsigned char inputNumber, void handlePress)
-    {
-        unsigned char newInState = PORTA & (1 << inputNumber); // select the 0th bit of port A
-        unsigned char oldInState = 0;
-        unsigned char debounceTimeout = 0;
-        if (debounceTimeout == 0)
-        { // if there has been enough time since the last press
-            if (newBtnState > oldBtnState)
-            { // if button pressed disable registering further presses for 100ms
-                debounceTimeout = 1;
-                handlePress(); // run the function
-                __delay_ms(100);
-                debounceTimeout = 0;
-            }
+void processInput(unsigned char inputNumber, unsigned char ledNumber)
+{
+    unsigned char newInState = (PORTA & (1 << inputNumber))>>inputNumber; // select the 0th bit of port A
+    unsigned char oldInState = 0;
+    unsigned char debounceTimeout = 0;
+    if (debounceTimeout == 0)
+    { // if there has been enough time since the last press
+        if (newInState > oldInState)
+        { // if button pressed disable registering further presses for 100ms
+            debounceTimeout = 1;
+            blink(ledNumber); // run the function
+            __delay_ms(100);
+            debounceTimeout = 0;
         }
     }
+}
 
-    void blink()
-    {
-        // turn on the last LED for half a second then turn off
-        LATB = 0xFFFE;
-        __delay_ms(500);
-        LATB = 0xFFFF;
-    }
+int main(void)
+{
+    AD1PCFGL = 0xFFFF; // set all to digital
+    TRISA = 0xFFFF; // set all to inputs
+    TRISB = 0x0000; // set all to outputs 
+    LATB = 0xFFFF; // all are off
 
     while (1)
     {
-        processInput(0, blink());
+        processInput(0, 0);
+        processInput(1, 1);
+        processInput(2, 2);
+        processInput(4, 3);
     }
     return 0;
 }
