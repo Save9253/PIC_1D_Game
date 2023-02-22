d2911f35925ab502306bd27947a6cb646d24ce2a
Саунин Савелий <save9253@mail.ru>
2023-02-22 14:15:08 -0500

    distributed the functions

--- newmainXC16.c
+++ newmainXC16.c
@@ -1,6 +1,6 @@
 /*
  * File:   newmainXC16.c
- * Author: savel
+ * Author: Saveliy Saunin
  *
  * Created on February 21, 2023, 8:06 PM
  */
@@ -9,32 +9,41 @@
 #define FCY 3700000UL
 #include <libpic30.h>
 
-int main(void) {
+int main(void)
+{
     AD1PCFGL = 0xFFFF;
     TRISA = 0xFFFF;
     TRISB = 0x0000;
-    LATB = 0xFFFF;// all are off
-    
-    unsigned char handlePress() {
-        //turn on the last LED for half a second then turn off
+    LATB = 0xFFFF; // all are off
+
+    void processInput(unsigned char inputNumber, void handlePress)
+    {
+        unsigned char newInState = PORTA & (1 << inputNumber); // select the 0th bit of port A
+        unsigned char oldInState = 0;
+        unsigned char debounceTimeout = 0;
+        if (debounceTimeout == 0)
+        { // if there has been enough time since the last press
+            if (newBtnState > oldBtnState)
+            { // if button pressed disable registering further presses for 100ms
+                debounceTimeout = 1;
+                handlePress(); // run the function
+                __delay_ms(100);
+                debounceTimeout = 0;
+            }
+        }
+    }
+
+    void blink()
+    {
+        // turn on the last LED for half a second then turn off
         LATB = 0xFFFE;
         __delay_ms(500);
         LATB = 0xFFFF;
     }
-    
-    while(1){
-        unsigned char newBtnState = PORTA & (1<<0);// select the 0th bit of port A
-        unsigned char oldBtnState = 0;
-        unsigned char debounceTimeout = 0;
-        if(debounceTimeout == 0){//if there has been enough time since the last press
-            if(newBtnState > oldBtnState){//if button pressed disable registering further presses for 100ms
-                debounceTimeout = 1;
-                handlePress();// run the function
-                __delay_ms(100);
-                debounceTimeout = 0;
-            }   
-        }
-        
+
+    while (1)
+    {
+        processInput(0, blink());
     }
     return 0;
 }
