1185c346309fc2d36032243e7f8ba3c55022a12f
Save <66020327+Save9253@users.noreply.github.com>
2023-02-22 11:38:45 -0500

    Added button debounce

--- newmainXC16.c
+++ newmainXC16.c
@@ -14,16 +14,27 @@ int main(void) {
     TRISA = 0xFFFF;
     TRISB = 0x0000;
     LATB = 0xFFFF;// all are off
+    
+    unsigned char handlePress() {
+        //turn on the last LED for half a second then turn off
+        LATB = 0xFFFE;
+        __delay_ms(500);
+        LATB = 0xFFFF;
+    }
+    
     while(1){
-        unsigned char inputs = PORTA & 0x0001;
-        //if(inputs == 0xFFFE){
-            if(inputs == 0x0001){
-                LATB = 0xFFFD;//1st on
-            }else{
-                LATB = 0xFFFE;//0th on
-            }
-            __delay_ms(100);
-        //}   
+        unsigned char newBtnState = PORTA & (1<<0);// select the 0th bit of port A
+        unsigned char oldBtnState = 0;
+        unsigned char debounceTimeout = 0;
+        if(debounceTimeout == 0){//if there has been enough time since the last press
+            if(newBtnState > oldBtnState){//if button pressed disable registering further presses for 100ms
+                debounceTimeout = 1;
+                handlePress();// run the function
+                __delay_ms(100);
+                debounceTimeout = 0;
+            }   
+        }
+        
     }
     return 0;
 }
